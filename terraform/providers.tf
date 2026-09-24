8285163c76441f86c6531b2d53ea0a0eba69f037
knagpal1803 <knagpal1803@gmail.com>
2026-09-24 14:37:07 +0100

    Add Docker and Grafana providers to Terraform config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 3.0.0"
+    }
+  }
+}
 
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+provider "grafana" {
+  url  = "http://localhost:3000"
+  auth = var.AUTH_VAR
+}
