75f649a1fdbd1a7a53f8847d7267d327d101d055
knagpal1803 <knagpal1803@gmail.com>
2026-09-24 14:52:34 +0100

    Add Docker network and Prometheus setup
    Added Docker network and Prometheus container configuration.

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -1,25 +1,40 @@
-resource "docker_volume" "grafana_storage" {
-  name = "grafana-storage"
+resource "docker_network" "monitoring_net" {
+  name = "monitoring_net"
 }
+
+resource "docker_image" "prometheus" {
+  name = "prom/prometheus:latest"
+}
+
+resource "docker_container" "prometheus" {
+  name  = "prometheus"
+  image = docker_image.prometheus.image_id
+  networks_advanced {
+    name = docker_network.monitoring_net.name
+  }
+  ports {
+    internal = 9090
+    external = 9090
+  }
+}
+
 resource "docker_image" "grafana" {
-  name         = "grafana/grafana:latest"
-  keep_locally = false
+  name = "grafana/grafana:latest"
 }
 
 resource "docker_container" "grafana" {
-  image = docker_image.grafana.image_id
   name  = "grafana"
-
+  image = docker_image.grafana.image_id
+  networks_advanced {
+    name = docker_network.monitoring_net.name
+  }
   ports {
     internal = 3000
     external = 3000
   }
-
-  # Set initial admin credentials
-  env = var.credentials
-
-  volumes {
-    volume_name    = docker_volume.grafana_storage.name
-    container_path = "/var/lib/grafana"
-  }
+  env = [
+    "GF_SECURITY_ADMIN_USER=admin",
+    "GF_SECURITY_ADMIN_PASSWORD=strongpassword123",
+    "GF_USERS_ALLOW_SIGN_UP=false"
+  ]
 }
