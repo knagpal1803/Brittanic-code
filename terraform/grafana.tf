3d76db38789df090a5c354456473d76a6e380e87
knagpal1803 <knagpal1803@gmail.com>
2026-09-24 14:53:37 +0100

    Refactor Grafana resources and add organization

--- terraform/grafana.tf
+++ terraform/grafana.tf
@@ -1,12 +1,46 @@
-resource "grafana_folder" "metrics_folder" {
-  title      = "Infrastructure Metrics"
-  depends_on = [docker_container.grafana]
+resource "grafana_organization" "main_org" {
+  name         = "Main Org."
+  admin_user   = "admin"
+  create_users = false
 }
 
 resource "grafana_data_source" "prometheus" {
+  org_id     = grafana_organization.main_org.org_id
   type       = "prometheus"
-  name       = "Local Prometheus"
-  url        = "http://localhost:9090"
+  name       = "Prometheus-Docker"
+  url        = "http://prometheus:9090" # Uses container name inside monitoring_net
   is_default = true
+
+  depends_on = [docker_container.grafana, docker_container.prometheus]
+}
+
+resource "grafana_folder" "infra_folder" {
+  org_id = grafana_organization.main_org.org_id
+  title  = "Infrastructure Metrics"
+
   depends_on = [docker_container.grafana]
+}
+
+resource "grafana_contact_point" "email_alerts" {
+  name   = "Email Team Platform"
+  org_id = grafana_organization.main_org.org_id
+
+  email {
+    addresses    = ["devops-team@example.com"]
+    single_email = true
+  }
+}
+
+resource "grafana_notification_policy" "alert_routing" {
+  org_id = grafana_organization.main_org.org_id
+
+  receiver = grafana_contact_point.email_alerts.name
+
+  policy {
+    receiver = grafana_contact_point.email_alerts.name
+    matchers {
+      label = "severity"
+      value = "critical"
+    }
+  }
 }
